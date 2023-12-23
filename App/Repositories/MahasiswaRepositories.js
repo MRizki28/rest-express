8e10532e00384ffe4b7de4d8de17f43005eb93b9
MRizki28 <muhammadrizkitkj123@gmail.com>
2023-12-23 20:57:52 +0800

    create function get data by id and update

--- App/Repositories/MahasiswaRepositories.js
+++ App/Repositories/MahasiswaRepositories.js
@@ -38,6 +38,50 @@ class MahasiswaRepositories {
         };
 
     }
+
+    async getDataById(request) {
+        const id = request.params.id
+        const data = await mahasiswaModel.findByPk(id);
+        if (!data || data.length === 0) {
+            return HttpResponseTraits.idOrDataNotFound();
+        } else {
+            return HttpResponseTraits.success(data);
+        }
+    }
+
+    async updateData(request) {
+        const id = request.params.id
+
+        const validation = Joi.object({
+            name: Joi.required().messages({
+                'any.required': ' Nama wajib diisi !'
+            }),
+        });
+
+        const { error } = validation.validate(request.body);
+
+        if (error) {
+            const errors = [error.message];
+            return HttpResponseTraits.checkValidation(errors)
+        }
+
+        try {
+            const { name } = request.body;
+            const data = await mahasiswaModel.findByPk(id);
+            if (!data) {
+                return HttpResponseTraits.idOrDataNotFound()
+            } else {
+                await data.update({
+                    name,
+                });
+            }
+
+            return HttpResponseTraits.success(data)
+        } catch (error) {
+            console.log(error);
+        };
+
+    }
 }
 
 module.exports = MahasiswaRepositories;
